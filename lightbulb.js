b159bde61a47f0984fc614e33ea993c228df9be1
Chels9897 <francheska.agaton@edu.sait.ca>
2026-01-13 12:19:20 -0700

    Lightbulb.js
    js now knows to change from off lightbulb to on

--- lightbulb.js
+++ lightbulb.js
@@ -16,3 +16,20 @@ document.querySelector("#light_on").addEventListener('click',function(){
     document.querySelector("#light_off").style.display="initial";
 })
 
+document.querySelector("#light_off").addEventListener('click', function(){
+//event listener for when the button is already on and gives the function to turn off the light
+   
+    document.querySelector("#status").textContent = "Lights are off";
+    document.querySelector("#lightbulb").src="lightbulb_off.webp";
+    document.querySelector("#lightbulb").alt="An illustration of a lightbulb off"
+
+
+    //change the background and foreground color
+
+    document.body.style.backgroundColor = "#000";
+    document.body.style.color = "#333";
+
+    document.querySelector("#light_off").style.display="none";
+    document.querySelector("#light_on").style.display="initial";
+
+})
